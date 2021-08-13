0f60f7d5e91a3d661dd84af33f79157a1d369a13
Matthew Wiessing <matthewwiessing@gmail.com>
2021-08-12 20:32:20 -0500

    added prompts for cases and numbers

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split(
 var uperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM".split("");
 var numbers = "1234567890".split("");
 var passwordLength = ""
+var askUper = true
+var askLower = true
+var askNumber = true
 
 //Get random Characters code
 function getRandomLowerCase() {
@@ -32,9 +35,14 @@ function writePassword() {
 function generatePassword() {
   var passwordLength = prompt (
 "How long do you want you password to be ?"
-  )
-  
+  );
+  var askUper = confirm("Do you want uppercase lettersin your password?")
+  var askNumber = confirm("Do you want numbers password?")
+  var askLower = confirm("Do you want lowercase letters in you password?")
 console.log(passwordLength)
+console.log(askUper)
+console.log(askNumber)
+console.log(askLower)
 }
 
 // Add event listener to generate button
