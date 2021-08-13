91fe91d92056ec10815c6bcb4b1836a3e4ef8b5a
Matthew Wiessing <matthewwiessing@gmail.com>
2021-08-12 19:25:20 -0500

    added character selecting code and strings

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split(
+  '');
+var uperCase = "QWERTYUIOPLKJHGFDSAZXCVBNM".split("");
+var numbers = "1234567890".split("");
 
+//Get random Characters code
+function getRandomLowerCase() {
+return lowerCase[Math.floor(Math.random() * lowerCase.length)]
+};
+function getRandomNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)]
+};
+function getRandomUperCase() {
+    return uperCase[Math.floor(Math.random() * uperCase.length)]
+};
+
+console.log(getRandomLowerCase());
+console.log(getRandomUperCase());
+console.log(getRandomNumber());
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +30,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
